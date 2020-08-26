7ddd0a91b49bf7f7bc2b2c34ab33925c585b2bf8
StevenLv <gogophersteven@gmail.com>
2020-08-26 11:08:45 +0800

    Update install_frpc.sh

--- install_frpc.sh
+++ install_frpc.sh
@@ -5,11 +5,11 @@
 rm -rf ~/frp
 mkdir -p ~/frp
 cd ~/frp 
-#wget https://github.com/fatedier/frp/releases/download/v0.33.0/frp_0.33.0_linux_arm.tar.gz
+wget https://github.com/fatedier/frp/releases/download/v0.33.0/frp_0.33.0_linux_arm.tar.gz
 
 # for test
-cp /home/pi/frp_0.33.0_linux_arm.tar.gz .
-sleep 10
+# cp /home/pi/frp_0.33.0_linux_arm.tar.gz .
+# sleep 10
 
 sudo mkdir -p /etc/frp
 sudo tar -xvf frp_0.33.0_linux_arm.tar.gz -C /etc/frp
